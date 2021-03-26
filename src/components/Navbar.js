00e4c5f3dcdb58d99bae55bb235bef2f1e3bd7e8
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-26 21:18:40 +0900

    hamburger button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,21 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  // click event
+  const {click, setClick} = useState(false);
+  //click function toggle click true/false
+  const handleClick = () => setClick(!click);
   return (
     <nav className="navbar">
       <div className="navbar-container">
+        {/* logo/title */}
         <Link to="/"  className="navbar-logo">
           TRVL <i className="fab fa-typo3"></i>  
         </Link>
+        {/* hamburger menu */}
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fass fa-times" : "fas fa-bars"} />
+        </div>
       </div>
     </nav>
   )
