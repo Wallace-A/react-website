ca1d6312fc2125dd188f3532dfb17d1959569e00
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-27 18:28:23 +0900

    card components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <Router>
         <Navbar></Navbar>
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component=
+          {Home} />
         </Switch>
     </Router>
     
