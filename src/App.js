e0d209676cca9ddfd5957ee140bdd59e0e4b40d0
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-27 19:03:56 +0900

    pages added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
     <Router>
         <Navbar></Navbar>
         <Switch>
-          <Route path="/" exact component=
-          {Home} />
+          <Route path="/" exact component={Home} />
+          <Route path="/services" exact component={Services} />
         </Switch>
     </Router>
     
