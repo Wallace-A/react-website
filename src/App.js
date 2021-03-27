9b31f11377894a65225e5d486853dc36c78a6574
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-27 19:14:06 +0900

    footer started

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/sign-up" exact component={SignUp} />
         </Switch>
     </Router>
     
